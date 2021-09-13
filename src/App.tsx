2e75e5afa0109d49726b88959d2b917557e1fdf0
Benjamin Bridges <benjaminbridges.wayne@gmail.com>
2021-09-12 21:42:04 -0500

    imports and api fetch
    I normally use axios but im curious about fetch. Might change later though

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,41 @@
+import { useState } from "react";
+import { useQuery } from "react-query";
+
+// Components to use
+import Drawer from '@material-ui/core/Drawer'
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Grid from '@material-ui/core/Grid';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart'
+import Badge from '@material-ui/core/Badge'
+
+// Fancy Styles
+import { Wrapper } from "./App.styles";
+
+// Cart Data Types
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+}
+
+
+
+const getProducts = async (): Promise<CartItemType[]> =>
+  await (await fetch('https://fakestoreapi.com/products')).json()
 
 const App = () => {
+  const {data, isLoading, error} = useQuery<CartItemType[]>(
+    'products', 
+    getProducts
+    );
+    console.log(data);
+    
   return (
-    <div className="App">
-      
-    </div>
+    <div className="App">Hi</div>
   );
 }
 
